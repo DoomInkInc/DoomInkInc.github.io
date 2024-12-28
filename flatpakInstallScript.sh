dc20063e54ef502067f99449f963bbe63930e76e
ItsDoomOne <85998702+ItsDoomOne@users.noreply.github.com>
2024-12-27 22:34:30 -0300

    i am actually going insane i shouldnt be coding this late without an energy drink

---
+++ flatpakInstallScript.sh
@@ -0,0 +1,8 @@
+#I am currently experiencing nightmares of a demonlike being calling me on WhatsApp. Please God help me, i am suffering.
+#i am being haunted for my past sins. please lord have mercy on me.
+sudo apt install flatpak
+sudo add-apt-repository ppa:flatpak/stable
+sudo apt update
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
